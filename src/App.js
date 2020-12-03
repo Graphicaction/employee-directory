9c58e444685de95d0c769191f559658cfe450578
Rashmi Sharma <r.s@chaione.com>
2020-12-03 13:35:00 -0500

    adding content for Fake Iframe

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
 import React from 'react';
-import EmployeeSearch from './components/EmployeeSearch';
 
 function App() {
+  const [message, setMessage] =  React.useState();
+
+  const sendUserInfoMessage = () => {
+    console.log('sending message')
+    window.parent.postMessage("user-info", "*");
+  };
+
+  const sendNotificationMessage = () => {
+    const payload = {
+      messageType: 'notification',
+      title: 'Random alert',
+      description: 'Some text'
+    };
+    console.log('sending message')
+    window.parent.postMessage(payload, "*");
+  };
+
+  const sendBadNotificationMessage = () => {
+    const payload = {
+      messageType: 'notification',
+      title: 1,
+      description: 'Some text'
+    };
+    console.log('sending message')
+    window.parent.postMessage(payload, "*");
+  };
+
+  const handleIncomingMessage = (event) => {
+    console.log(event.data);
+    setMessage(JSON.stringify(event.data));
+  };
+
+  React.useEffect(() => {
+    window.addEventListener("message", handleIncomingMessage);
+    return () => window.removeEventListener("message", handleIncomingMessage)
+  }, [handleIncomingMessage]);
+
   return (
-    <EmployeeSearch />
+    <div className="App">
+      <header className="App-header">
+        <button onClick={sendUserInfoMessage}>
+          Click to send user-info message
+        </button>
+        <button onClick={sendNotificationMessage}>
+          Click to create a notification
+        </button>
+        <button onClick={sendBadNotificationMessage}>
+          Click to create a bad notification
+        </button>
+        {message}
+      </header>
+    </div>
   );
 }
 
